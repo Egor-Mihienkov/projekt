fd4f85e664e5fbd591b61e0e54b9b749d4c3d20c
Egor <example@gmail.com>
2021-08-09 19:43:55 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+console.log('arr'+"- objekt");
+
+let incr = 10,
+    decr = 10;
+
+  
+    
+
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(5%2);
+
+    console.log(5*2 == 10);
+
+    const isCheked = true,
+          isClose = true;
+          console.log(isCheked && isClose);
