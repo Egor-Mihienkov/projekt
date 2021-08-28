60f1d49b84c4fd00ccc0ace92e85fe5b090ec8b9
Egor <example@gmail.com>
2021-08-28 17:35:20 +0300

    пр3

---
+++ строки и числа.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const str = "text";
+
+console.log(str.toUpperCase());
+console.log(str);
+
+const arr = [1, 2, 4];
+
+console.log(arr.length);
+
+
+const str = "text";
+
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const legg = "Hello world";
+
+console.log(legg.slice(6, 11));
+
+const num = 124.5;
+
+console.log(Math.round(num));
